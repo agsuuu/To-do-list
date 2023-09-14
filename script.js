377174b108b072299e1ebe65f700ef24fab55d6d
John Mark <johnmarkagsangre@gmail.com>
2023-09-14 23:01:41 +0800

    started js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Retrieve todo from local storage or initialize an empty array
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
