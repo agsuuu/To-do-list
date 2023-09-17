7dd26c2aa5bd9df4ee5b2e706c61945a4c64eed4
John Mark <johnmarkagsangre@gmail.com>
2023-09-17 23:12:44 +0800

    added new display function

--- script.js
+++ script.js
@@ -29,3 +29,25 @@ document.addEventListener("DOMContentLoaded", function () {
       displayTasks();
     }
   }
+
+  function displayTasks() {
+    todoList.innerHTML = "";
+    todo.forEach((item, index) => {
+      const p = document.createElement("p");
+      p.innerHTML = `
+        <div class="todo-container">
+          <input type="checkbox" class="todo-checkbox" id="input-${index}" ${
+        item.disabled ? "checked" : ""
+      }>
+          <p id="todo-${index}" class="${
+        item.disabled ? "disabled" : ""
+      }" onclick="editTask(${index})">${item.text}</p>
+        </div>
+      `;
+      p.querySelector(".todo-checkbox").addEventListener("change", () =>
+        toggleTask(index)
+      );
+      todoList.appendChild(p);
+    });
+    todoCount.textContent = todo.length;
+  }
